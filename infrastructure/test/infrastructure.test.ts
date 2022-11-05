27dbe1d06da8aabde349587d5ec67e4e2a268cc0
Niel McLaren <d@nielmclaren.com>
2022-11-04 21:50:18 -0700

    Update test.

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,11 +1,16 @@
 import * as cdk from "aws-cdk-lib";
 import { Template, Match } from "aws-cdk-lib/assertions";
 import * as CodeConveyor from "../lib/codeconveyor-stack";
+import { DevEnvironmentSettings } from "../lib/environment-settings";
 
 test("SQS Queue and SNS Topic Created", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new CodeConveyor.CodeConveyorStack(app, "MyTestStack");
+  const stack = new CodeConveyor.CodeConveyorStack(
+    app,
+    "MyTestStack",
+    new DevEnvironmentSettings()
+  );
   // THEN
 
   const template = Template.fromStack(stack);
