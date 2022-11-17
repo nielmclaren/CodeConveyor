17fef8dafe51cc86983a5ccb41db48d35fbe1a40
Niel McLaren <d@nielmclaren.com>
2022-11-16 23:43:57 -0800

    Create A records for REST API custom domain.

--- infrastructure/lib/codeconveyor-stack.ts
+++ infrastructure/lib/codeconveyor-stack.ts
@@ -1,11 +1,15 @@
 import {
+  aws_apigateway,
   aws_certificatemanager,
+  aws_iam,
+  aws_lambda,
+  aws_lambda_nodejs,
   CfnOutput,
+  CustomResource,
+  custom_resources,
   Stack,
   StackProps,
 } from "aws-cdk-lib";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as apigw from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
 import { IEnvironmentSettings } from "./environment-settings";
 
@@ -20,9 +24,9 @@ export class CodeConveyorStack extends Stack {
   ) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, "HelloHandler", {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset("lambda"),
+    const hello = new aws_lambda.Function(this, "HelloHandler", {
+      runtime: aws_lambda.Runtime.NODEJS_14_X,
+      code: aws_lambda.Code.fromAsset("lambda"),
       handler: "hello.handler",
     });
 
@@ -32,14 +36,59 @@ export class CodeConveyorStack extends Stack {
       envSettings.certificateArn
     );
 
-    const restApi = new apigw.LambdaRestApi(this, "CodeConveyorApiEndpoint", {
-      handler: hello,
-      domainName: {
-        certificate,
-        domainName: envSettings.domainName,
-      },
-    });
+    const restApi = new aws_apigateway.LambdaRestApi(
+      this,
+      "CodeConveyorApiEndpoint",
+      {
+        handler: hello,
+        domainName: {
+          certificate,
+          domainName: envSettings.apiDomainName,
+        },
+      }
+    );
+
+    this.createAliasRecords(envSettings, restApi);
 
     this.urlOutput = new CfnOutput(this, "Url", { value: restApi.url });
+  }
+
+  private createAliasRecords(
+    envSettings: IEnvironmentSettings,
+    restApi: aws_apigateway.LambdaRestApi
+  ): void {
+    const onEventHandler = new aws_lambda_nodejs.NodejsFunction(
+      this,
+      "AliasRecord", // Associate with Lambda code in codeconveyor-stack.AliasRecord.ts.
+      {
+        initialPolicy: [
+          new aws_iam.PolicyStatement({
+            actions: ["sts:AssumeRole"],
+            resources: ["*"],
+          }),
+        ],
+      }
+    );
+    const provider = new custom_resources.Provider(
+      this,
+      `AliasRecordProvider-CodeConveyor${envSettings.environmentName}`,
+      {
+        onEventHandler,
+      }
+    );
+    new CustomResource(
+      this,
+      `AliasRecordCustomResource-CodeConveyor${envSettings.environmentName}`,
+      {
+        serviceToken: provider.serviceToken,
+        properties: {
+          zoneName: envSettings.apiDomainName,
+          domain: envSettings.apiDomainName,
+          targetAlias: restApi.domainName?.domainNameAliasDomainName,
+          targetHostedZoneId: restApi.domainName?.domainNameAliasHostedZoneId,
+          delegationRoleArn: envSettings.hostedZoneDelegationRoleArn,
+        },
+      }
+    );
   }
 }
--- infrastructure/lib/environment-settings.ts
+++ infrastructure/lib/environment-settings.ts
@@ -1,22 +1,37 @@
 export interface IEnvironmentSettings {
+  apiDomainName: string;
+  apiHostedZoneId: string;
   certificateArn: string;
-  domainName: string;
+  environmentName: string;
+  hostedZoneDelegationRoleArn: string;
 }
 
 export class DevEnvironmentSettings {
+  public apiDomainName = "codeconveyor-api-dev.nielmclaren.com";
+  public apiHostedZoneId = "Z0680943349ZC50NSIHSH";
   public certificateArn =
     "arn:aws:acm:us-west-2:510650182850:certificate/441a45e2-d5e7-46ff-9c8f-0e8e71b23cd0";
-  public domainName = "codeconveyor-api-dev.nielmclaren.com";
+  public environmentName = "Dev";
+  public hostedZoneDelegationRoleArn =
+    "arn:aws:iam::695408758741:role/Route53NSRecordCreationRole-CodeConveyorDev";
 }
 
 export class StagingEnvironmentSettings {
+  public apiDomainName = "codeconveyor-api-staging.nielmclaren.com";
+  public apiHostedZoneId = "Z004505022VU1UZSVXS9O";
   public certificateArn =
     "arn:aws:acm:us-west-2:933497984357:certificate/d2909517-9af4-45ed-a4da-03a804a9cdae";
-  public domainName = "codeconveyor-api-staging.nielmclaren.com";
+  public environmentName = "Staging";
+  public hostedZoneDelegationRoleArn =
+    "arn:aws:iam::695408758741:role/Route53NSRecordCreationRole-CodeConveyorStaging";
 }
 
 export class ProdEnvironmentSettings {
+  public apiDomainName = "codeconveyor-api.nielmclaren.com";
+  public apiHostedZoneId = "Z07262761AJ5JYX85WQC9";
   public certificateArn =
     "arn:aws:acm:us-west-2:858585152460:certificate/0116a88b-23f6-4d9c-939a-6e052202ea26";
-  public domainName = "codeconveyor-api.nielmclaren.com";
+  public environmentName = "Prod";
+  public hostedZoneDelegationRoleArn =
+    "arn:aws:iam::695408758741:role/Route53NSRecordCreationRole-CodeConveyorProd";
 }
