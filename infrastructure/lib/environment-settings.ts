36816db469ebf150cbbb2fb40c166b254c70f9ed
Niel McLaren <d@nielmclaren.com>
2022-11-18 16:19:19 -0800

    Update delegation role names.

--- infrastructure/lib/environment-settings.ts
+++ infrastructure/lib/environment-settings.ts
@@ -13,7 +13,7 @@ export class DevEnvironmentSettings {
     "arn:aws:acm:us-west-2:510650182850:certificate/441a45e2-d5e7-46ff-9c8f-0e8e71b23cd0";
   public environmentName = "Dev";
   public hostedZoneDelegationRoleArn =
-    "arn:aws:iam::695408758741:role/Route53NSRecordCreationRole-CodeConveyorDev";
+    "arn:aws:iam::695408758741:role/CodeConveyorDevHostedZoneDelegationRole";
 }
 
 export class StagingEnvironmentSettings {
@@ -23,7 +23,7 @@ export class StagingEnvironmentSettings {
     "arn:aws:acm:us-west-2:933497984357:certificate/d2909517-9af4-45ed-a4da-03a804a9cdae";
   public environmentName = "Staging";
   public hostedZoneDelegationRoleArn =
-    "arn:aws:iam::695408758741:role/Route53NSRecordCreationRole-CodeConveyorStaging";
+    "arn:aws:iam::695408758741:role/CodeConveyorStagingHostedZoneDelegationRole";
 }
 
 export class ProdEnvironmentSettings {
@@ -33,5 +33,5 @@ export class ProdEnvironmentSettings {
     "arn:aws:acm:us-west-2:858585152460:certificate/0116a88b-23f6-4d9c-939a-6e052202ea26";
   public environmentName = "Prod";
   public hostedZoneDelegationRoleArn =
-    "arn:aws:iam::695408758741:role/Route53NSRecordCreationRole-CodeConveyorProd";
+    "arn:aws:iam::695408758741:role/CodeConveyorProdHostedZoneDelegationRole";
 }
