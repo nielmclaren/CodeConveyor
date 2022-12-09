29a669ba7785c98a1f30f36f795ca76449e58ab6
Niel McLaren <d@nielmclaren.com>
2022-12-09 12:41:10 -0800

    Change CodePipeline depending on branch name.

--- infrastructure/lib/PipelineStack.ts
+++ infrastructure/lib/PipelineStack.ts
@@ -1,3 +1,4 @@
+import assert = require("assert");
 import * as cdk from "aws-cdk-lib";
 import { SecretValue } from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
@@ -10,15 +11,23 @@ export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const branchName = this.node.tryGetContext("branchName");
+    assert(typeof branchName === "string");
+
     const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "CodeConveyorPipeline",
       crossAccountKeys: true,
 
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("nielmclaren/CodeConveyor", "main", {
+        input: CodePipelineSource.gitHub("nielmclaren/CodeConveyor", branchName, {
           authentication: SecretValue.secretsManager("CodeConveyorGithubToken"),
         }),
-        commands: ["cd infrastructure", "yarn install --frozen-lockfile", "yarn build", "npx cdk synth"],
+        commands: [
+          "cd infrastructure",
+          "yarn install --frozen-lockfile",
+          "yarn build",
+          `npx cdk synth -c branchName=${branchName}`,
+        ],
         primaryOutputDirectory: "infrastructure/cdk.out",
       }),
     });
@@ -29,16 +38,18 @@ export class PipelineStack extends cdk.Stack {
       })
     );
 
-    pipeline.addStage(
-      new CodeConveyorStage(this, "Staging", new StagingEnvironmentSettings(), {
-        env: { account: REFLECT_STAGING_ACCOUNT, region: "us-west-2" },
-      })
-    );
+    if (branchName === "main") {
+      pipeline.addStage(
+        new CodeConveyorStage(this, "Staging", new StagingEnvironmentSettings(), {
+          env: { account: REFLECT_STAGING_ACCOUNT, region: "us-west-2" },
+        })
+      );
 
-    pipeline.addStage(
-      new CodeConveyorStage(this, "Prod", new ProdEnvironmentSettings(), {
-        env: { account: REFLECT_PROD_ACCOUNT, region: "us-west-2" },
-      })
-    );
+      pipeline.addStage(
+        new CodeConveyorStage(this, "Prod", new ProdEnvironmentSettings(), {
+          env: { account: REFLECT_PROD_ACCOUNT, region: "us-west-2" },
+        })
+      );
+    }
   }
 }
